6bff57754bc194d00f7b719a3bd480b82a8eb7e1
ilyakharin <ilya2021511@gmail.com>
2021-06-04 20:12:36 +0300

    f6

---
+++ rumireaharinpractice6/room/src/main/java/com/example/room/Employee.java
@@ -0,0 +1,13 @@
+package com.example.room;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Employee {
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+    public String name;
+    public int salary;
+}
